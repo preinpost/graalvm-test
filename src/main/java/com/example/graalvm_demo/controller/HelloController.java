1c37f253342ec33d14c566401559dc2074a0f8c1
minsoo.lee <minsoo.lee@8byte.co.kr>
2023-06-08 13:57:25 +0900

    init

---
+++ src/main/java/com/example/graalvm_demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.graalvm_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+}
